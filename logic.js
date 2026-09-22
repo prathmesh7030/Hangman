88a4b6a15d0a987e64907e40872107cee895c705
Prathmesh7030 <kolpeprathmesh9960@gmail.com>
2026-09-22 23:03:50 +0530

    Show hidden word

--- logic.js
+++ logic.js
@@ -16,3 +16,29 @@ let playable = true;
 
 const correctLetters = [];
 const wrongLetters = [];
+
+// Show hidden word
+function displayWord() {
+	wordEl.innerHTML = `
+    ${selectedWord
+			.split('')
+			.map(
+				letter => `
+          <span class="letter">
+            ${correctLetters.includes(letter) ? letter : ''}
+          </span>
+        `
+			)
+			.join('')}
+  `;
+
+	const innerWord = wordEl.innerText.replace(/[ \n]/g, '');
+
+	if (innerWord === selectedWord) {
+		finalMessage.innerText = 'Congratulations! You won! 😃';
+		finalMessageRevealWord.innerText = '';
+		popup.style.display = 'flex';
+
+		playable = false;
+	}
+}
