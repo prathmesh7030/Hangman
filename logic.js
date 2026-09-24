e988aa675499f4792f3a2c1d249f7c70c18bd264
Prathmesh7030 <kolpeprathmesh9960@gmail.com>
2026-09-24 22:06:56 +0530

    Empty arrays

--- logic.js
+++ logic.js
@@ -111,3 +111,18 @@ window.addEventListener('keydown', e => {
 // Restart game and play again
 playAgainBtn.addEventListener('click', () => {
 	playable = true;
+
+    //  Empty arrays
+	correctLetters.splice(0);
+	wrongLetters.splice(0);
+
+	selectedWord = words[Math.floor(Math.random() * words.length)];
+
+	displayWord();
+
+	updateWrongLettersEl();
+
+	popup.style.display = 'none';
+});
+
+displayWord();	
